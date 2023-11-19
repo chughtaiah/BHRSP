b63b3cda9f944365ab86685df269c30bac168f60
chughtaiah <81219064+chughtaiah@users.noreply.github.com>
2023-11-19 16:24:49 +0500

    Mesh

--- Point Cloud Registration/AngularError.m
+++
@@ -1,5 +0,0 @@
-function err = AngularError(R_pre, R_gt)
-
-err = abs(acos((trace(R_pre'*R_gt)-1)/2));
-
-end
--- Point Cloud Registration/plot_build_environment.m
+++
@@ -1,6 +0,0 @@
-Environment(2000,.0001,.7,1,1);
-
-set(gcf,'color','w');
-
-grid off;
-axis off;
